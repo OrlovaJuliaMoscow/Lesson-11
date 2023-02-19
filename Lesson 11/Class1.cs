699a332c5e1c14b8c943b0213c9f4e70c3a40e08
orlova.julia.ivan@gmail.com <orlova.julia.ivan@gmail.com>
2023-02-19 23:49:45 +0300

    Добавьте файлы проекта.

---
+++ Lesson 11/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_11
+{
+    struct Linear
+    {
+        double k;
+        double b;
+        
+        public Linear(double k, double b)
+        {
+
+            this.k = k;
+            this.b = b;
+           
+        }
+
+        public string Root()
+        {
+            double x = -b / k;
+       
+        return $"solution - {x}";
+        }
+ 
+    }
+}
---
+++ Lesson 11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double k= Convert.ToDouble(Console.ReadLine()); 
+            double b= Convert.ToDouble(Console.ReadLine()); 
+           
+            Linear linear = new Linear (k, b);
+            Console.WriteLine(linear.Root());
+            Console.ReadKey();
+        }
+        
+    }
+}
